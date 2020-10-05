04e693b72a8f18ceb2c8eb9994fb51d2c9139d99
spark7453 <sython@naver.com>
2020-10-05 23:49:50 +0900

    Add docstring example

---
+++ docstring.py
@@ -0,0 +1,15 @@
+def add_one(x: int) -> int:
+    """
+    Returns input value added one
+
+    :param x: int
+
+    :return: x + 1
+
+    :except x less than 0:
+
+    """
+    assert x >= 0
+    return x + 1
+
+print(add_one(0))
