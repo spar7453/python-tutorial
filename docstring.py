870204bbbd4370f9291c92f12a9f3600ad7e1d4b
spark7453 <sython@naver.com>
2020-10-06 00:03:47 +0900

    Change param docstring

--- docstring.py
+++ docstring.py
@@ -2,12 +2,9 @@ def add_one(x: int) -> int:
     """
     Returns input value added one
 
-    :param x: int
-
+    :param x: value greater than or equal to 0
     :return: x + 1
-
     :except x less than 0:
-
     """
     assert x >= 0
     return x + 1
